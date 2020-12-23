7ec7cc71b340f7c6bc2325770e9d49f9c657eb7f
Emma Donovan <emmadonovan@Emmas-MacBook-Pro.local>
2020-12-23 16:04:07 +0000

    Updated button colour and bio width

--- src/components/EmailMe.jsx
+++ src/components/EmailMe.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import {Button} from "react-bootstrap";
 
 class EmailMe extends React.Component {
     render() {
         return (
             <div class="contact-me">
             <h2>Get In Touch</h2>
-            <a class="btn" href="mailto:emmalilydonovan@gmail.com">EMAIL ME</a>
+            {/* <a class="btn" href="mailto:emmalilydonovan@gmail.com">EMAIL ME</a> */}
+            <Button variant="primary" size="lg" href="mailto:emmalilydonovan@gmail.com">
+                EMAIL ME
+            </Button>
         </div>
         );
     }
